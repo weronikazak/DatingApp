6b045358b6f69785b77b8af890615eb7c6d2a1a5
Weronika Żak <weronikarzak@gmail.com>
2019-06-28 22:22:08 +0200

    Login Users

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specity password between 4 and 8 signs")]
         public string Password { get; set; }
     }
 }
