1d8853814b14a766e2b453f987fbf78985e31747
Weronika Żak <weronikarzak@gmail.com>
2019-06-29 23:12:03 +0200

    add navbar, fix bootstrap, angular service

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,23 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { MaterialIconComponent } from './material-icon/material-icon.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ValueComponent,
-    MaterialIconComponent
+    MaterialIconComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
