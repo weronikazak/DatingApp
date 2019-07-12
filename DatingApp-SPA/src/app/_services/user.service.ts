ce4567be47746b1f8995a4e66b18f2562f8c1270
Weronika Żak <weronikarzak@gmail.com>
2019-07-12 17:57:28 +0200

    user can edit profile manually

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -3,6 +3,7 @@ import { environment } from '../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
+import { MemberCardComponent } from '../members/member-card/member-card.component';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -24,5 +25,9 @@ export class UserService {
 
   getUser(id): Observable<User> {
     return this.http.get<User>(this.baseUrl + 'user/' + id, httpOptions);
+  }
+
+  updateUser(id: MemberCardComponent, user: User) {
+    return this.http.put<User>(this.baseUrl + 'user/' + id, user);
   }
 }
--- DatingApp.API/Controllers/UserController.cs
+++ DatingApp.API/Controllers/UserController.cs
@@ -5,6 +5,8 @@ using DatingApp.API.Dtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Security.Claims;
+using System;
 
 namespace DatingApp.API.Controllers
 {
@@ -35,6 +37,22 @@ namespace DatingApp.API.Controllers
             var userToReturn = _mapper.Map<UserForDetailedDto>(user);
 
             return Ok(userToReturn);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto){
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+ 
+
+            var userFromRepo = await _repo.GetUser(id);
+
+            _mapper.Map(userForUpdateDto, userFromRepo);
+
+            if (await _repo.SaveAll())
+                return NoContent();
+
+            throw new Exception($"Updating user {id} failed on save");
         }
     }
 }
