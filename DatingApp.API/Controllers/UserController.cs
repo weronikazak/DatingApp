2bc5a30649655342a50fc3996fc342f4999fcba7
Weronika Żak <weronikarzak@gmail.com>
2019-07-20 16:02:46 +0200

    create paging helpers

--- DatingApp.API/Controllers/UserController.cs
+++ DatingApp.API/Controllers/UserController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Security.Claims;
 using System;
+using DatingApp.API.Helpers;
 
 namespace DatingApp.API.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [Authorize]
     [Route("api/[controller]")]
     [ApiController]
